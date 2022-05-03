b5d3ae62d735206c06ecfe2dbfd2a424929f435a
Khoi Ky <kky@webmd.net>
2022-05-03 13:55:57 -0700

    Fixing tests that are not in Pester5 format.

--- Tests/Import-Carbon.FileSystem.Tests.ps1
+++ Tests/Import-Carbon.FileSystem.Tests.ps1
@@ -2,50 +2,52 @@
 #Requires -Version 5.1
 Set-StrictMode -Version 'Latest'
 
-& (Join-Path -Path $PSScriptRoot -ChildPath 'Initialize-Test.ps1' -Resolve)
+BeforeAll{
+    & (Join-Path -Path $PSScriptRoot -ChildPath 'Initialize-Test.ps1' -Resolve)
 
-function GivenModuleLoaded
-{
-    Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\Carbon.FileSystem\Carbon.FileSystem.psd1' -Resolve)
-    Get-Module -Name 'Carbon.FileSystem' | Add-Member -MemberType NoteProperty -Name 'NotReloaded' -Value $true
+    function GivenModuleLoaded
+    {
+        Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\Carbon.FileSystem\Carbon.FileSystem.psd1' -Resolve)
+        Get-Module -Name 'Carbon.FileSystem' | Add-Member -MemberType NoteProperty -Name 'NotReloaded' -Value $true
+    }
+    
+    function GivenModuleNotLoaded
+    {
+        Remove-Module -Name 'Carbon.FileSystem' -Force -ErrorAction Ignore
+    }
+    
+    function Init
+    {
+    
+    }
+    
+    function ThenModuleLoaded
+    {
+        $module = Get-Module -Name 'Carbon.FileSystem'
+        $module | Should -Not -BeNullOrEmpty
+        $module | Get-Member -Name 'NotReloaded' | Should -BeNullOrEmpty
+    }
+    
+    function WhenImporting
+    {
+        $script:importedAt = Get-Date
+        Start-Sleep -Milliseconds 1
+        & (Join-Path -Path $PSScriptRoot -ChildPath '..\Carbon.FileSystem\Import-Carbon.FileSystem.ps1' -Resolve)
+    }
 }
 
-function GivenModuleNotLoaded
-{
-    Remove-Module -Name 'Carbon.FileSystem' -Force -ErrorAction Ignore
-}
-
-function Init
-{
-
-}
-
-function ThenModuleLoaded
-{
-    $module = Get-Module -Name 'Carbon.FileSystem'
-    $module | Should -Not -BeNullOrEmpty
-    $module | Get-Member -Name 'NotReloaded' | Should -BeNullOrEmpty
-}
-
-function WhenImporting
-{
-    $script:importedAt = Get-Date
-    Start-Sleep -Milliseconds 1
-    & (Join-Path -Path $PSScriptRoot -ChildPath '..\Carbon.FileSystem\Import-Carbon.FileSystem.ps1' -Resolve)
-}
-
-Describe 'Import-Carbon.FileSystem.when module not loaded' {
-    It 'should import the module' {
+Describe 'Import-Carbon.FileSystem' {
+    BeforeEach {
         Init
+    }
+
+    It 'should import the module' {
         GivenModuleNotLoaded
         WhenImporting
         ThenModuleLoaded
     }
-}
 
-Describe 'Import-Carbon.FileSystem.when module loaded' {
     It 'should re-import the module' {
-        Init
         GivenModuleLoaded
         WhenImporting
         ThenModuleLoaded
